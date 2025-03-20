964c001459dac0d751ffc333a69273c21a4016c6
Bogdan Gusiev <agresso@gmail.com>
2025-03-20 18:22:41 +0100

    Display capture buttons even if session is not running

--- Sources/Internal/UI/Default Screens/Camera/DefaultCameraScreen+BottomBar.swift
+++ Sources/Internal/UI/Default Screens/Camera/DefaultCameraScreen+BottomBar.swift
@@ -90,8 +90,8 @@ private extension DefaultCameraScreen.BottomBar {
     var changeCameraPositionButtonIconColor: Color { .init(.mijickBackgroundInverted) }
 }
 private extension DefaultCameraScreen.BottomBar {
-    var isOutputTypeSwitchActive: Bool { parent.config.cameraOutputSwitchAllowed && parent.cameraManager.captureSession.isRunning && !parent.isRecording }
-    var isLightButtonActive: Bool { parent.config.lightButtonAllowed && parent.hasLight && parent.cameraManager.captureSession.isRunning && !parent.isRecording }
-    var isCaptureButtonActive: Bool { parent.config.captureButtonAllowed && parent.cameraManager.captureSession.isRunning }
-    var isChangeCameraPositionButtonActive: Bool { parent.config.cameraPositionButtonAllowed && parent.cameraManager.captureSession.isRunning && !parent.isRecording }
+    var isOutputTypeSwitchActive: Bool { parent.config.cameraOutputSwitchAllowed && !parent.isRecording }
+    var isLightButtonActive: Bool { parent.config.lightButtonAllowed && parent.hasLight &&  !parent.isRecording }
+    var isCaptureButtonActive: Bool { parent.config.captureButtonAllowed }
+    var isChangeCameraPositionButtonActive: Bool { parent.config.cameraPositionButtonAllowed && !parent.isRecording }
 }
