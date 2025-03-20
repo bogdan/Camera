866e3e38a6a691fbd1688ff325e203b5f670173b
Bogdan Gusiev <agresso@gmail.com>
2025-03-20 20:26:58 +0100

    do

--- Sources/Internal/Manager/CameraManager+NotificationCenter.swift
+++ Sources/Internal/Manager/CameraManager+NotificationCenter.swift
@@ -10,6 +10,7 @@
 
 
 import Foundation
+import UIKit
 
 @MainActor class CameraManagerNotificationCenter {
     private(set) var parent: CameraManager!
@@ -20,6 +21,23 @@ extension CameraManagerNotificationCenter {
     func setup(parent: CameraManager) {
         self.parent = parent
         NotificationCenter.default.addObserver(self, selector: #selector(handleSessionWasInterrupted), name: .AVCaptureSessionWasInterrupted, object: parent.captureSession)
+//        NotificationCenter.default.addObserver(self, selector: #selector(pauseSession), name: UIApplication.didEnterBackgroundNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(resumeSession), name: UIApplication.willEnterForegroundNotification, object: nil)
+
+    }
+    
+    
+//    @objc private func pauseSession() {
+//        parent.captureSession.stopRunning() 
+//    }
+
+    @objc private func resumeSession() {
+        let session = parent.captureSession
+        DispatchQueue.global(qos: .userInitiated).async {
+            if !session.isRunning {
+                session.startRunning() // Restart session when app enters foreground
+            }
+        }
     }
 }
 private extension CameraManagerNotificationCenter {
