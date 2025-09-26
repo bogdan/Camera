e867bffdbef04b54b1ccb6b01450317926a1fb4d
Bogdan Gusiev <agresso@gmail.com>
2025-09-26 14:31:56 +0200

    Add front and back device properties to MCameraScreen
    - Introduced `frontDevice` and `backDevice` properties to access the front and back camera devices respectively.
    - These properties retrieve the device from `frontCameraInput` and `backCameraInput` of `cameraManager`.

--- Sources/Public/UI/Public+UI+MCameraScreen.swift
+++ Sources/Public/UI/Public+UI+MCameraScreen.swift
@@ -224,6 +224,8 @@ public extension MCameraScreen {
     var isGridVisible: Bool { cameraManager.attributes.isGridVisible }
     
     var device: CaptureDevice? { cameraManager.getCameraInput()?.device }
+    var frontDevice: CaptureDevice? { cameraManager.frontCameraInput?.device }
+    var backDevice: CaptureDevice? { cameraManager.backCameraInput?.device }
 
     var minAvailableVideoZoomFactor: CGFloat { cameraManager.getCameraInput()?.device.minAvailableVideoZoomFactor ?? 1 }
     var maxAvailableVideoZoomFactor: CGFloat { cameraManager.getCameraInput()?.device.maxAvailableVideoZoomFactor ?? 1 }
